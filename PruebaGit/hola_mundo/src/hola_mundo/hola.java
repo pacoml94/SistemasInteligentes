b8b36aadda27ac5a3d5f6e7c06be75eaed4476f4
Francisco <Fco.Muriillo@alu.uclm.es>
2016-10-18 16:13:01 +0200

    OK

--- PruebaGit/hola_mundo/src/hola_mundo/hola.java
+++ PruebaGit/hola_mundo/src/hola_mundo/hola.java
@@ -3,8 +3,8 @@ package hola_mundo;
 class hola {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("El primer commit funciona");
+		System.out.println("OK");
 	}
 
 }
