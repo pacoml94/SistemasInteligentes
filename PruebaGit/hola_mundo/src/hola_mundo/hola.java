e9b949689d35ee241c398938801f0b6a6f33c016
Francisco <Fco.Muriillo@alu.uclm.es>
2016-10-18 16:18:40 +0200

    bucle for de prueba

--- PruebaGit/hola_mundo/src/hola_mundo/hola.java
+++ PruebaGit/hola_mundo/src/hola_mundo/hola.java
@@ -5,6 +5,10 @@ class hola {
 	public static void main(String[] args) {
 		System.out.println("El primer commit funciona");
 		System.out.println("OK");
+		
+		for(int i = 0; i<=9; i++) {
+			System.out.printf("Valor de i: %d", i);
+		}
 	}
 
 }
