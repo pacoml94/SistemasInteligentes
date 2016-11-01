c044b8ee92731e040c7e91bb34ad230da5c5b5dd
Francisco <Fco.Murillo@alu.uclm.es>
2016-11-01 12:15:45 +0100

    Paquete puzzle nuevo

---
+++ PuzzleSInteligentes/Puzzle/src/Puzzle_Nuevo/Puzzle.java
@@ -0,0 +1,43 @@
+package Puzzle_Nuevo;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Puzzle {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Puzzle window = new Puzzle();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Puzzle() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 400, 400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
