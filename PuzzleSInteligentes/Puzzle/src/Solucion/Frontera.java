7ab96481ab4c639bbde31b8ae94e3f51cd60afbf
pacoml94 <Fco.Murillo@alu.uclm.es>
2016-11-15 16:19:53 +0100

    Clase fronter inicializada

--- PuzzleSInteligentes/Puzzle/src/Solucion/Frontera.java
+++ PuzzleSInteligentes/Puzzle/src/Solucion/Frontera.java
@@ -1,5 +1,13 @@
 package Solucion;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Frontera {
 
+    private List <Nodo> frontera;
+    
+    public Frontera() {
+        this.frontera = new ArrayList<>();
+    }
 }
