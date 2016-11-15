bed11264507df270878952698f61d6e187dfa16c
pacoml94 <Fco.Murillo@alu.uclm.es>
2016-11-15 16:02:50 +0100

    Clase nodo implementada

--- PuzzleSInteligentes/Puzzle/src/Solucion/Nodo.java
+++ PuzzleSInteligentes/Puzzle/src/Solucion/Nodo.java
@@ -1,5 +1,55 @@
 package Solucion;
 
 public class Nodo {
+    
+    private int profundidad;
+    private int id;
+    private int f;
+    private int costo;
+    private int h;
+    
+    public Nodo() {
+        
+    }
+
+    public int getProfundidad() {
+        return profundidad;
+    }
+
+    public void setProfundidad(int profundidad) {
+        this.profundidad = profundidad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getF() {
+        return f;
+    }
+
+    public void setF(int f) {
+        this.f = f;
+    }
+
+    public int getCosto() {
+        return costo;
+    }
+
+    public void setCosto(int costo) {
+        this.costo = costo;
+    }
+
+    public int getH() {
+        return h;
+    }
+
+    public void setH(int h) {
+        this.h = h;
+    }
 
 }
